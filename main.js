a9b2bb15db7119d60e00d08a1449c27c43cad674
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-07-20 17:45:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let totalComida = 2800 + 1300;
+
+let totalBebida = 900 + 650;
+
+let total = totalComida + totalBebida;
+
+console.log("El total de comida es: " + totalComida);
+console.log("El total de bebida es: " + totalBebida);
+console.log("El total de la cuenta es: " + total);
